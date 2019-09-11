c21746b557a80c4c6d61e8cff47bcd8756034cc2
AClumsy <510415008@qq.com>
2019-09-11 22:46:57 +0800

    init code

---
+++ src/views/control/index.js
@@ -0,0 +1,8 @@
+import RoleList from './role/List'
+import AuditList from './audit/List'
+import MenuList from './menu/List'
+import MenuDetails from './menu/Details'
+import AdminList from './admin/List'
+import PublicApiList from './publicApi/List'
+
+export { RoleList, AuditList, AdminList, PublicApiList, MenuList, MenuDetails }
